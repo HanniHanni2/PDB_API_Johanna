bcd9eb62560f893c830b2775156e6626d4ce72b1
orca <arvidarvidlundqvist@gmail.com>
2025-03-27 17:19:04 +0100

    Omvandla PDB-Id till PDB_files

--- dataApi.py
+++ dataApi.py
@@ -1,23 +1,22 @@
-
 import os
-import csv
-from Bio import PDB
+import requests  # installera requests "pip install requests"
+
+def download_pdb(pdb_ids, save_dir= "/home/ocra/Documents/pdb_files"): ##skapa mappen pdb_files själv för hand. byt till rätt user
+    base_url = "https://files.rcsb.org/download/{}.pdb"
 
 
-def read_pdb_ids_csv(csv_file):
-    pdb_ids = []
-    try:
-        with open(csv_file, mode='r') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                pdb_ids.append(row[PDB_ID])
-    except Exception as e:
-        print(f"Fel vid inläsning av CSV-filen: {e}")
-    return pdb_ids
 
-csv_file = 'sal.csv'
-pdb_ids = read_pdb_ids_from_csv(csv_file)
+    for pdb_id in pdb_ids:
+        pdb_id =pdb_id.strip().upper()
+        url= base_url.format(pdb_id)
+        response= requests.get(url)
 
-output_dir = "pdb_files"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+        if response.status_code == 200:
+            file_path = os.path.join(save_dir, f"{pdb_id}.pdb")
+            with open(file_path, "wb") as f:
+                f.write(response.content)
+        else: print("failed")
+
+
+pdb_list= ["1CRN", "4HHB"] ##byt mot önskad lista
+download_pdb(pdb_list)
